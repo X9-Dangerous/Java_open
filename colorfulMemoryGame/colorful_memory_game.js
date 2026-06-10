659229cbe00efdc346072daa7ea9ba14f0f9241e
K-Owiti <kowiticesd@gmail.com>
2026-06-10 09:56:49 -0400

    JavaScript part of colorfulmemorygame

---
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -0,0 +1,44 @@
+function generateCards() {
+    for (const color of cards) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.color = color;
+        card.textContent = '?';
+        gameContainer.appendChild(card);
+    }
+}
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+function startGame() {
+    let timeLeft = 30;
+    startbtn.disabled = true;
+    score = 0; // Reset score to zero
+    scoreElement.textContent = `Score: ${score}`;
+    startGameTimer(timeLeft);
+    cards = shuffle(colors.concat(colors));
+    selectedCards = [];
+    gameContainer.innerHTML = '';
+    generateCards();
+    gameContainer.addEventListener('click', handleCardClick);
+}
+function startGameTimer(timeLeft) {
+    timerElement.textContent = `Time Left: ${timeLeft}`;
+    gameInterval = setInterval(() => {
+        timeLeft--;
+        timerElement.textContent = `Time Left: ${timeLeft}`;
+
+        if (timeLeft === 0) {
+            clearInterval(gameInterval);
+            let timeLeft = 30;
+            alert('Game Over!');
+            startbtn.disabled = false;
+        }
+    }, 1000);
+}
+startbtn.addEventListener('click', startGame);
+
